84c93d7ba4a4f4c0ee3dc7b8ae0ab65dc1536067
redthing1 <redthing1@alt.icu>
2025-07-01 04:39:50 -0700

    cmake refactor

---
+++ cmake/TracerConfig.cmake
@@ -0,0 +1,48 @@
+# tracer library configuration
+include_guard()
+
+include(${CMAKE_SOURCE_DIR}/cmake/CommonConfig.cmake)
+
+# create tracer library targets (shared and static)
+function(create_tracer_targets tracer_name source_files)
+    if(WITNESS_BUILD_SHARED)
+        add_library(${tracer_name}_qbdipreload SHARED ${source_files})
+        configure_tracer_target(${tracer_name}_qbdipreload ${tracer_name})
+    endif()
+
+    if(WITNESS_BUILD_STATIC)
+        add_library(${tracer_name}_static STATIC ${source_files})        
+        configure_tracer_target(${tracer_name}_static ${tracer_name})
+    endif()
+endfunction()
+
+# configure individual tracer target
+function(configure_tracer_target target_name tracer_name)
+    target_include_directories(${target_name} PRIVATE
+        ${CMAKE_SOURCE_DIR}/src
+        ${CMAKE_SOURCE_DIR}/src/third_party/qbdi/tools/QBDIPreload/include
+    )
+
+    target_link_libraries(${target_name} PRIVATE
+        w1tn3ss
+        QBDI_static
+        redlog::redlog
+    )
+    
+    # link qbdipreload for shared library version
+    if(${target_name} MATCHES "_qbdipreload$")
+        target_link_libraries(${target_name} PRIVATE QBDIPreload)
+    endif()
+
+    apply_common_compile_options(${target_name})
+    apply_windows_definitions(${target_name})
+    set_standard_output_dirs(${target_name})
+
+    # remove lib prefix and set macos rpath
+    set_target_properties(${target_name} PROPERTIES PREFIX "")
+    if(APPLE)
+        set_target_properties(${target_name} PROPERTIES MACOSX_RPATH TRUE)
+    elseif(UNIX)
+        target_link_libraries(${target_name} PRIVATE dl)
+    endif()
+endfunction()
