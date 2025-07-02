da3f1768e9564ea22bce1b256b9e7225425e1170
redthing1 <redthing1@alt.icu>
2025-07-01 21:07:45 -0700

    fix w1trace output

--- src/tracers/w1trace/trace_config.hpp
+++ src/tracers/w1trace/trace_config.hpp
@@ -14,7 +14,7 @@ struct trace_config {
     w1::util::env_config loader("W1TRACE_");
 
     trace_config config;
-    config.output_file = loader.get<std::string>("OUTPUT_FILE", "trace.txt");
+    config.output_file = loader.get<std::string>("OUTPUT", "trace.txt");
     config.buffer_size = static_cast<size_t>(loader.get<uint64_t>("BUFFER_SIZE", 256 * 1024 * 1024));
     config.verbose = loader.get<bool>("VERBOSE", false);
 
