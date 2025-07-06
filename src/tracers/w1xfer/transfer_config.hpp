6eabb022f3ae7532eee79805d7fda30dcb98a15f
redthing1 <redthing1@alt.icu>
2025-07-05 23:14:23 -0700

    fixup! tracers: handle verbose properly

--- src/tracers/w1xfer/transfer_config.hpp
+++ src/tracers/w1xfer/transfer_config.hpp
@@ -20,7 +20,6 @@ struct transfer_config {
     config.verbose = loader.get<int>("VERBOSE", 0);
     config.output_file = loader.get<std::string>("OUTPUT", "transfers.json");
     config.max_entries = loader.get<uint64_t>("MAX_ENTRIES", 1000000);
-    config.verbose = loader.get<bool>("VERBOSE", false);
     config.log_registers = loader.get<bool>("LOG_REGISTERS", true);
     config.log_stack_info = loader.get<bool>("LOG_STACK_INFO", true);
     config.log_call_targets = loader.get<bool>("LOG_CALL_TARGETS", true);
