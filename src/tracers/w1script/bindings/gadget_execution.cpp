c6c56def017e355bb326e6b41a8c5decc40ad2eb
redthing1 <redthing1@alt.icu>
2025-07-14 14:19:37 -0700

    gadgeting: fix final reg state

--- src/tracers/w1script/bindings/gadget_execution.cpp
+++ src/tracers/w1script/bindings/gadget_execution.cpp
@@ -78,13 +78,62 @@ void setup_gadget_execution(
             result_table["error"] = result.error;
           }
 
-          // add final register state (simplified - just a few key registers)
+          // add final register state
           sol::table gpr_table = lua.create_table();
+#if defined(__aarch64__) || defined(_M_ARM64)
           gpr_table["x0"] = result.gpr.x0;
           gpr_table["x1"] = result.gpr.x1;
           gpr_table["x2"] = result.gpr.x2;
           gpr_table["x3"] = result.gpr.x3;
+          gpr_table["x4"] = result.gpr.x4;
+          gpr_table["x5"] = result.gpr.x5;
+          gpr_table["x6"] = result.gpr.x6;
+          gpr_table["x7"] = result.gpr.x7;
+          gpr_table["x8"] = result.gpr.x8;
+          gpr_table["x9"] = result.gpr.x9;
+          gpr_table["x10"] = result.gpr.x10;
+          gpr_table["x11"] = result.gpr.x11;
+          gpr_table["x12"] = result.gpr.x12;
+          gpr_table["x13"] = result.gpr.x13;
+          gpr_table["x14"] = result.gpr.x14;
+          gpr_table["x15"] = result.gpr.x15;
+          gpr_table["x16"] = result.gpr.x16;
+          gpr_table["x17"] = result.gpr.x17;
+          gpr_table["x18"] = result.gpr.x18;
+          gpr_table["x19"] = result.gpr.x19;
+          gpr_table["x20"] = result.gpr.x20;
+          gpr_table["x21"] = result.gpr.x21;
+          gpr_table["x22"] = result.gpr.x22;
+          gpr_table["x23"] = result.gpr.x23;
+          gpr_table["x24"] = result.gpr.x24;
+          gpr_table["x25"] = result.gpr.x25;
+          gpr_table["x26"] = result.gpr.x26;
+          gpr_table["x27"] = result.gpr.x27;
+          gpr_table["x28"] = result.gpr.x28;
+          gpr_table["x29"] = result.gpr.x29;
+          gpr_table["lr"] = result.gpr.lr;
           gpr_table["sp"] = result.gpr.sp;
+          gpr_table["pc"] = result.gpr.pc;
+#elif defined(__x86_64__) || defined(_M_X64)
+          gpr_table["rax"] = result.gpr.rax;
+          gpr_table["rbx"] = result.gpr.rbx;
+          gpr_table["rcx"] = result.gpr.rcx;
+          gpr_table["rdx"] = result.gpr.rdx;
+          gpr_table["rsi"] = result.gpr.rsi;
+          gpr_table["rdi"] = result.gpr.rdi;
+          gpr_table["r8"] = result.gpr.r8;
+          gpr_table["r9"] = result.gpr.r9;
+          gpr_table["r10"] = result.gpr.r10;
+          gpr_table["r11"] = result.gpr.r11;
+          gpr_table["r12"] = result.gpr.r12;
+          gpr_table["r13"] = result.gpr.r13;
+          gpr_table["r14"] = result.gpr.r14;
+          gpr_table["r15"] = result.gpr.r15;
+          gpr_table["rbp"] = result.gpr.rbp;
+          gpr_table["rsp"] = result.gpr.rsp;
+          gpr_table["rip"] = result.gpr.rip;
+          gpr_table["eflags"] = result.gpr.eflags;
+#endif
           result_table["gpr"] = gpr_table;
 
           return result_table;
