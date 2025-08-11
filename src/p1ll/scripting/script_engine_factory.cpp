d9dcff2eb2455488bcced82c7839d2c86fcb7316
redthing1 <redthing1@alt.icu>
2025-08-10 21:34:56 -0700

    engine factory: awareness of supported engine

--- src/p1ll/scripting/script_engine_factory.cpp
+++ src/p1ll/scripting/script_engine_factory.cpp
@@ -10,29 +10,27 @@
 
 namespace p1ll::scripting {
 
-std::unique_ptr<IScriptEngine> ScriptEngineFactory::create() {
-  return create(get_default_engine_type());
-}
+std::unique_ptr<IScriptEngine> ScriptEngineFactory::create() { return create(get_default_engine_type()); }
 
 std::unique_ptr<IScriptEngine> ScriptEngineFactory::create(ScriptEngineType engine_type) {
   auto log = redlog::get_logger("p1ll.script_factory");
-  
+
   switch (engine_type) {
 #ifndef PILL_SCRIPT_ENGINE_JS
-    case ScriptEngineType::LUA:
-      log.dbg("creating lua script engine");
-      return std::make_unique<lua::lua_engine>();
+  case ScriptEngineType::LUA:
+    log.dbg("creating lua script engine");
+    return std::make_unique<lua::lua_engine>();
 #endif
-      
+
 #ifdef PILL_SCRIPT_ENGINE_JS
-    case ScriptEngineType::JAVASCRIPT:
-      log.inf("creating js script engine");
-      return std::make_unique<js::js_engine>();
+  case ScriptEngineType::JAVASCRIPT:
+    log.inf("creating js script engine");
+    return std::make_unique<js::js_engine>();
 #endif
-      
-    default:
-      log.err("unsupported script engine type for this build configuration");
-      return nullptr;
+
+  default:
+    log.err("unsupported script engine type for this build configuration");
+    return nullptr;
   }
 }
 
@@ -44,6 +42,49 @@ ScriptEngineType ScriptEngineFactory::get_default_engine_type() {
   // default to lua
   return ScriptEngineType::LUA;
 #endif
+}
+
+std::vector<ScriptEngineType> ScriptEngineFactory::get_supported_engines() {
+  std::vector<ScriptEngineType> supported;
+
+#ifndef PILL_SCRIPT_ENGINE_JS
+  supported.push_back(ScriptEngineType::LUA);
+#endif
+
+#ifdef PILL_SCRIPT_ENGINE_JS
+  supported.push_back(ScriptEngineType::JAVASCRIPT);
+#endif
+
+  return supported;
+}
+
+std::vector<std::string> ScriptEngineFactory::get_supported_extensions() {
+  std::vector<std::string> extensions;
+
+#ifndef PILL_SCRIPT_ENGINE_JS
+  extensions.push_back(".lua");
+#endif
+
+#ifdef PILL_SCRIPT_ENGINE_JS
+  extensions.push_back(".js");
+#endif
+
+  return extensions;
+}
+
+bool ScriptEngineFactory::is_engine_supported(ScriptEngineType engine_type) {
+  switch (engine_type) {
+#ifndef PILL_SCRIPT_ENGINE_JS
+  case ScriptEngineType::LUA:
+    return true;
+#endif
+#ifdef PILL_SCRIPT_ENGINE_JS
+  case ScriptEngineType::JAVASCRIPT:
+    return true;
+#endif
+  default:
+    return false;
+  }
 }
 
 } // namespace p1ll::scripting
--- src/p1ll/scripting/script_engine_factory.hpp
+++ src/p1ll/scripting/script_engine_factory.hpp
@@ -2,6 +2,8 @@
 
 #include "script_engine.hpp"
 #include <memory>
+#include <vector>
+#include <string>
 
 namespace p1ll::scripting {
 
@@ -10,12 +12,21 @@ class ScriptEngineFactory {
 public:
   // create script engine based on compile-time configuration
   static std::unique_ptr<IScriptEngine> create();
-  
+
   // create script engine by explicit type (for testing)
   static std::unique_ptr<IScriptEngine> create(ScriptEngineType engine_type);
-  
+
   // get the default engine type based on build configuration
   static ScriptEngineType get_default_engine_type();
+
+  // get list of supported script engines in this build
+  static std::vector<ScriptEngineType> get_supported_engines();
+
+  // get file extensions for supported engines
+  static std::vector<std::string> get_supported_extensions();
+
+  // check if an engine type is supported in this build
+  static bool is_engine_supported(ScriptEngineType engine_type);
 };
 
 } // namespace p1ll::scripting
